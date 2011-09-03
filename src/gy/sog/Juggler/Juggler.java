f27cc40d19c04dc7cd18dbb9456242371e3cf1a3
Emile Snyder <esnyder@hp.(none)>
2011-09-03 15:35:14 -0700

    Added logging to UDP send exceptions, commented out bluetooth to stop crashing.

--- src/gy/sog/Juggler/Juggler.java
+++ src/gy/sog/Juggler/Juggler.java
@@ -33,8 +33,8 @@ public class Juggler extends Activity implements SensorListener
         }
 
         outView.setText(String.format("hello world... from CODE %f", 2.0f));
-        Bluetooth b = new Bluetooth();
-        b.startServer();
+        //Bluetooth b = new Bluetooth();
+        //b.startServer();
        
     }
 
@@ -60,15 +60,17 @@ public class Juggler extends Activity implements SensorListener
     public void sendAccelData(String server, int port, String msgStr) {
 	try {
 	    DatagramSocket s = new DatagramSocket();
-	    InetAddress local = InetAddress.getByName(server);
+	    InetAddress saddr = InetAddress.getByName(server);
 	    int msg_length=msgStr.length();
 	    byte[] message = msgStr.getBytes();
-	    DatagramPacket p = new DatagramPacket(message, msg_length,local,port);
+	    DatagramPacket p = new DatagramPacket(message, msg_length,saddr,port);
 	    s.send(p);
 	} catch (SocketException e) {
-	    // arg!
+	    Log.d("sendAccelData", "SocketException: " + e);
 	} catch (UnknownHostException e) {
+	    Log.d("sendAccelData", "HostException: " + e);
 	} catch (java.io.IOException e) {
+	    Log.d("sendAccelData", "IOException: " + e);
 	}
     }
 }
